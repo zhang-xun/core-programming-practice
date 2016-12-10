13b725c49f6ca44dd0628f1ffb1cade8d743155b
zhangxun <15025689012@163.com>
2016-12-10 19:36:38 +0800

    press any key to continue Enter must press  ---> todo : anykey to contiue

---
+++ chapter9/9-4-25_more_show.py
@@ -0,0 +1,26 @@
+#! /usr/local/bin/python3
+# _*_ coding:utf-8 _*_ 
+
+def show(filename):
+    """
+        25 lines stop util any key pressed!
+        input: file of a name
+        output: show text
+    """
+    flag = 0
+    with open(filename,"r") as f:
+        for line in f:
+            
+            if flag == 25: 
+                input("press any key to continue")
+                flag = 0
+                continue
+            else:
+                print(line.strip())
+                flag+=1
+                
+
+
+if __name__ == "__main__":
+    filename = input("please input filename:")
+    show(filename)

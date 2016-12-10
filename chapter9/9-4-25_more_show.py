7ef60883e9112ae16c4752bda40750477d5dff39
zhangxun <15025689012@163.com>
2016-12-10 19:44:02 +0800

    press any key with shell :os.system('read -s -n -p commits')

--- chapter9/9-4-25_more_show.py
+++ chapter9/9-4-25_more_show.py
@@ -1,6 +1,7 @@
 #! /usr/local/bin/python3
 # _*_ coding:utf-8 _*_ 
 
+import os
 def show(filename):
     """
         25 lines stop util any key pressed!
@@ -12,7 +13,7 @@ def show(filename):
         for line in f:
             
             if flag == 25: 
-                input("press any key to continue")
+                os.system("read -s -n 1 -p 'press any key to continue '")
                 flag = 0
                 continue
             else:
